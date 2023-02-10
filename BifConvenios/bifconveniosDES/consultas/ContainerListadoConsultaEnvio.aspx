d541b34342b99bb344b521c6ecd9349c23149228
Juansh <juansh.herrera@gmail.com>
2023-02-09 19:27:10 -0500

    DEV: Cambio de crystal x rdlc

--- BifConvenios/bifconveniosDES/consultas/ContainerListadoConsultaEnvio.aspx
+++ BifConvenios/bifconveniosDES/consultas/ContainerListadoConsultaEnvio.aspx
@@ -1,15 +1,19 @@
 <%@ Page Language="vb" Inherits="BIFConvenios.ContainerListadoConsultaEnvio" AutoEventWireup="false" CodeFile="ContainerListadoConsultaEnvio.aspx.vb" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
 <%@ Register TagPrefix="uc1" TagName="Banner" Src="../controls/Banner.ascx" %>
-<%@ Register TagPrefix="cr" Namespace="CrystalDecisions.Web" Assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" %>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<HTML>
-	<HEAD>
+<%--<%@ Register TagPrefix="cr" Namespace="CrystalDecisions.Web" Assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" %>--%>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
 		<title>BIFConvenios - Listado de Importes por Vencer</title>
-		<meta name="GENERATOR" content="Microsoft Visual Studio.NET 7.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
+		<%--<meta name="GENERATOR" content="Microsoft Visual Studio.NET 7.0">
 		<meta name="CODE_LANGUAGE" content="Visual Basic 7.0">
-		<meta name="vs_defaultClientScript" content="JavaScript">
-		<LINK href="<%=Request.ApplicationPath%>/css/global.css" type=text/css rel=stylesheet>
-		<meta name="vs_targetSchema" content="http://schemas.microsoft.com/intellisense/ie5">
+		<meta name="vs_defaultClientScript" content="JavaScript">--%>
+		<LINK href="<%=Request.ApplicationPath%>/css/global.css" type="text/css" rel="stylesheet">
+		<%--<meta name="vs_targetSchema" content="http://schemas.microsoft.com/intellisense/ie5">--%>
 		
 		<script type="text/javascript">
 		/* ADD NCA 08/07/2014 EA2013-273 OPT. PROCESOS CONVENIOS */
@@ -19,7 +23,7 @@
 			}
 		/* END */	
 		</script>
-	</HEAD>
+	</head>
 	<body leftMargin="0" topMargin="0" rightMargin="0">
 		<form id="Form1" method="post" runat="server">
 			<table cellSpacing="0" cellPadding="0" width="100%" border="0">
@@ -53,6 +57,11 @@
 			<%--MIGRAR INNOVA FALTA
 				<CR:CrystalReportViewer id="CrystalReportViewer1" runat="server" DisplayGroupTree="False" EnableDrillDown="False" HasDrillUpButton="False" HasGotoPageButton="False" HasSearchButton="False" HasToggleGroupTreeButton="False" Height="50px" Width="350px"></CR:CrystalReportViewer>--%>
 			<!-- ADD NCA 08/07/2014 EA2013-273 OPT. CONVENIOS  -->
+			<asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+			<rsweb:ReportViewer ID="ReportViewer1" runat="server" BackColor="" ClientIDMode="AutoID" ProcessingMode="Local" Width="100%" Height="700px" ShowDocumentMapButton="false" DocumentMapCollapsed="true">
+                <LocalReport ReportPath="consultas\RepListadoCuotasPorVencer.rdlc">
+                </LocalReport>
+            </rsweb:ReportViewer>
 			<input id="hdId" type="hidden" name="hdId" runat="server"/>
 			<!-- END -->
 		</form>
--- BifConvenios/bifconveniosDES/consultas/ContainerListadoConsultaEnvio.aspx.vb
+++ BifConvenios/bifconveniosDES/consultas/ContainerListadoConsultaEnvio.aspx.vb
@@ -12,12 +12,14 @@
 ' - Nombre  : export
 ' - Descripcion: Determina si se realiza exportacion directa del formato
 
-Imports CrystalDecisions.CrystalReports.Engine
+'Imports CrystalDecisions.CrystalReports.Engine
 '******************************************************
 
 Imports System.Data.SqlClient
 Imports System.IO
 Imports CrystalDecisions.Shared
+Imports Microsoft.Reporting.WebForms
+
 Namespace BIFConvenios
 
     Public Class ContainerListadoConsultaEnvio
@@ -53,93 +55,126 @@ Namespace BIFConvenios
 
         Private Sub Page_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-            'ADD: AAZ - 2017/12/06 - Incidencia 26
-            'Inicializamos e instanciamos el objeto CrystalReportViewer, DataSet
-            Dim oRepListadoCuotasPorVencer As New RepListadoCuotasPorVencer()
-            Dim ds As New DataSetListadoCuotasFin()
-            'FIN ADD
-
-            If Request.Params("idp") Is Nothing Then
-                If (Not Session.Item("Info") Is Nothing) Then
-
-                    ds = CType(Session.Item("Info"), DataSetListadoCuotasFin)
-                    'ds.WriteXml("C:/DATASET.TXT")
-
-                    oRepListadoCuotasPorVencer.SetDataSource(ds)
-
-                    'ADD NCA 08/07/2014 EA2013-273 OPT. PROCESOS CONVENIOS - ARCHIVOS DESCUENTOS
-                    If Not (Session.Item("idx") Is Nothing) Then hdId.Value = Session.Item("idx").ToString
-                    'END
-
-                End If
-            Else
-
-                idp = Request.Params("idp")
-                'ADD NCA 08/07/2014 EA2013-273 OPT. PROCESOS CONVENIOS - ARCHIVOS DESCUENTOS
-                hdId.Value = idp
-                'END
-
-                'Session.Add("Info", ArchivosDescuento.getDataSetListadoCuotasFin(idp))
-                ds = CType(ArchivosDescuento.getDataSetListadoCuotasFin(idp, "-", "-"), DataSetListadoCuotasFin)
-
-                'ds.WriteXml("C:/DATASET.TXT")
-                oRepListadoCuotasPorVencer.SetDataSource(ds)
-
-                Dim oProceso As New BIFConvenios.Proceso()
-                oProceso.UpdateFechaObtencionArchivo(idp, False, Context.User.Identity.Name)
-                oProceso.UpdEstadoGeneracionExito(idp, Context.User.Identity.Name)
-
-            End If
-
-            If Request.Params("export") Is Nothing Then
-                'MIGRAR INNOVA FALTA
-                'CrystalReportViewer1.ReportSource = oRepListadoCuotasPorVencer
-
-            Else
-
-                Dim name As String = getWebServerDateId() & ".xls"
-                Dim filename As String = Server.MapPath(".") & "\export\" & name
-                RemoveFiles(Server.MapPath(".") & "\export\", New TimeSpan(0, 6, 0, 0))
-
-                With oRepListadoCuotasPorVencer.ExportOptions
-                    .ExportDestinationType = ExportDestinationType.DiskFile
-                    .ExportFormatType = ExportFormatType.Excel
-                    .DestinationOptions = New CrystalDecisions.Shared.DiskFileDestinationOptions()
-                    .DestinationOptions.DiskFileName = filename
-                End With
-
-                oRepListadoCuotasPorVencer.Export()
+            If Not Page.IsPostBack Then
                 'ADD: AAZ - 2017/12/06 - Incidencia 26
-                'Cerramos y limpiamos el Objeto Crystal
-                oRepListadoCuotasPorVencer.Close()
-                oRepListadoCuotasPorVencer.Dispose()
+                'Inicializamos e instanciamos el objeto CrystalReportViewer, DataSet
+                'Dim oRepListadoCuotasPorVencer As New RepListadoCuotasPorVencer()
+                Dim ds As New DataSetListadoCuotasFin()
+                Dim rdsDlinfo As New ReportDataSource()
+                rdsDlinfo.Name = "DLinfo"
+                Dim rdsCliente As New ReportDataSource()
+                rdsCliente.Name = "Cliente"
                 'FIN ADD
 
-                Dim ms As System.IO.MemoryStream
-                Dim fileS As FileStream = File.OpenRead(filename)
-
-                ms = New MemoryStream(fileS.Length)
-                Dim br As BinaryReader = New BinaryReader(fileS)
-                Dim bytesRead As Byte() = br.ReadBytes(fileS.Length)
-
-                ms.Write(bytesRead, 0, fileS.Length)
-
                 If Request.Params("idp") Is Nothing Then
+                    If (Not Session.Item("Info") Is Nothing) Then
 
-                    With HttpContext.Current.Response
-                        .ClearContent()
-                        .ClearHeaders()
-                        .ContentType = "application/vnd.ms-excel"
-                        .AddHeader("Content-Disposition", "inline; filename=" & getWebServerDateId() & ".xls")
-                        .BinaryWrite(ms.ToArray)
+                        ds = CType(Session.Item("Info"), DataSetListadoCuotasFin)
+                        'ds.WriteXml("C:/DATASET.TXT")
 
-                        .End()
-                    End With
+                        'oRepListadoCuotasPorVencer.SetDataSource(ds)
+                        If Not IsNothing(ds) Then
+                            If ds.Tables.Count > 0 Then
+                                rdsDlinfo.Value = ds.Tables(0)
+                                rdsCliente.Value = ds.Tables(1)
+                            End If
+                        End If
+                        'ADD NCA 08/07/2014 EA2013-273 OPT. PROCESOS CONVENIOS - ARCHIVOS DESCUENTOS
+                        If Not (Session.Item("idx") Is Nothing) Then hdId.Value = Session.Item("idx").ToString
+                        'END
+
+                    End If
                 Else
-                    Response.Redirect("EnvioReporteMail.aspx?idP=" + idp + "&archivo=export\" & name)
+
+                    idp = Request.Params("idp")
+                    'ADD NCA 08/07/2014 EA2013-273 OPT. PROCESOS CONVENIOS - ARCHIVOS DESCUENTOS
+                    hdId.Value = idp
+                    'END
+
+                    'Session.Add("Info", ArchivosDescuento.getDataSetListadoCuotasFin(idp))
+                    ds = CType(ArchivosDescuento.getDataSetListadoCuotasFin(idp, "-", "-"), DataSetListadoCuotasFin)
+
+                    'ds.WriteXml("C:/DATASET.TXT")
+                    ' oRepListadoCuotasPorVencer.SetDataSource(ds)
+                    If Not IsNothing(ds) Then
+                        If ds.Tables.Count > 0 Then
+                            rdsDlinfo.Value = ds.Tables(0)
+                            rdsCliente.Value = ds.Tables(1)
+                        End If
+                    End If
+                    Dim oProceso As New BIFConvenios.Proceso()
+                    oProceso.UpdateFechaObtencionArchivo(idp, False, Context.User.Identity.Name)
+                    oProceso.UpdEstadoGeneracionExito(idp, Context.User.Identity.Name)
+
                 End If
 
+                If Request.Params("export") Is Nothing Then
+                    'MIGRAR INNOVA FALTA
+                    'CrystalReportViewer1.ReportSource = oRepListadoCuotasPorVencer
+                    If Not IsNothing(ds) Then
+                        If ds.Tables.Count > 0 Then
+                            rdsDlinfo.Value = ds.Tables(0)
+                            rdsCliente.Value = ds.Tables(1)
+                        End If
+                    End If
+                    ReportViewer1.LocalReport.DataSources.Add(rdsDlinfo)
+                    ReportViewer1.LocalReport.DataSources.Add(rdsCliente)
+                Else
+
+                    Dim name As String = getWebServerDateId() & ".xls"
+                    Dim filename As String = Server.MapPath(".") & "\export\" & name
+                    RemoveFiles(Server.MapPath(".") & "\export\", New TimeSpan(0, 6, 0, 0))
+
+                    'With oRepListadoCuotasPorVencer.ExportOptions
+                    '    .ExportDestinationType = ExportDestinationType.DiskFile
+                    '    .ExportFormatType = ExportFormatType.Excel
+                    '    .DestinationOptions = New CrystalDecisions.Shared.DiskFileDestinationOptions()
+                    '    .DestinationOptions.DiskFileName = filename
+                    'End With
+
+                    'oRepListadoCuotasPorVencer.Export()
+                    ''ADD: AAZ - 2017/12/06 - Incidencia 26
+                    ''Cerramos y limpiamos el Objeto Crystal
+                    'oRepListadoCuotasPorVencer.Close()
+                    'oRepListadoCuotasPorVencer.Dispose()
+                    'FIN ADD
+
+                    ReportViewer1.LocalReport.DataSources.Add(rdsDlinfo)
+                    ReportViewer1.LocalReport.DataSources.Add(rdsCliente)
+                    Dim mimeType As String
+                    Dim encoding As String
+                    Dim extension As String
+                    Dim streamIds As String()
+                    Dim warnings As Warning()
+                    Dim bytes = ReportViewer1.LocalReport.Render("Excel", Nothing, mimeType, encoding, extension, streamIds, warnings)
+
+                    'Dim ms As System.IO.MemoryStream
+                    'Dim fileS As FileStream = File.OpenRead(filename)
+
+                    'ms = New MemoryStream(fileS.Length)
+                    'Dim br As BinaryReader = New BinaryReader(fileS)
+                    'Dim bytesRead As Byte() = br.ReadBytes(fileS.Length)
+
+                    'ms.Write(bytesRead, 0, fileS.Length)
+
+                    If Request.Params("idp") Is Nothing Then
+
+                        With HttpContext.Current.Response
+                            .ClearContent()
+                            .ClearHeaders()
+                            .ContentType = "application/vnd.ms-excel"
+                            .AddHeader("Content-Disposition", "inline; filename=" & getWebServerDateId() & ".xls")
+                            .BinaryWrite(bytes)
+
+                            .End()
+                        End With
+                    Else
+                        Response.Redirect("EnvioReporteMail.aspx?idP=" + idp + "&archivo=export\" & name)
+                    End If
+
+                End If
             End If
+
 
         End Sub
 
